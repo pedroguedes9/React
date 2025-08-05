53c559adfb2028298baf049abaef103ff90e3bb9
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-08-05 13:09:32 -0300

    git config --global core.autocrlf true

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,7 +1,12 @@
+import Circle, {Square} from "@/components/forms"
+
 function Page() {
-  return <div>
+  return (<div>
     <h1 className="bg-amber-600 text-4xl">Olá</h1>
     <h2>mundo</h2>
-  </div>
+    <Square></Square>
+    <Circle></Circle>
+  </div>)
+
 }
 export default Page

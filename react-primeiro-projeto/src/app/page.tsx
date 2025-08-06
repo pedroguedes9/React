bcf70d3eeeeccb9ca372fc4d574e7bd4782de96c
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-08-05 21:06:19 -0300

    variáveis e etc

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,10 +1,11 @@
 import { GeoComponentes } from "@/components/geoComponentes"
-
+import { Person } from "@/components/person"
 function Page() {
   return (<div>
     <h1 className="bg-amber-600 text-4xl">Olá</h1>
     <h2>mundo</h2>
     <GeoComponentes/>
+    <Person/>
   </div>)
 
 }
--- react-primeiro-projeto/src/components/person.tsx
+++ react-primeiro-projeto/src/components/person.tsx
@@ -1,8 +1,33 @@
+function getWeekDay() {
+    return new Intl.DateTimeFormat(`pt-br`,{weekday: "long"}).format(new Date())
+}
+
 export const Person = () => {
+    const data: {name:string, lastName:string, avatar:string, roles: string[]} =  {
+        name: "Elon",
+        lastName: "Musk",
+        avatar: "#",
+        roles: [
+            "CEO da Tesla", "CEO da SpaceX"
+        ]
+    }
+    
     return (
         <>
-            <h1>Elon Musk</h1>
-            <img src="#" alt="Elon Musk" />
+            <h1 style={{color: "red", fontSize: "30px"}} >
+                Nome: {`
+                        ${data.name} ${data.lastName}
+                    `}</h1>
+            <img 
+                src={data.avatar} 
+                alt={`
+                    ${data.name} ${data.lastName}
+                `} />
+            <ul>
+                <li>{data.roles[0]}</li>
+                <li>{data.roles[1]}</li>
+            </ul>
+            <h1>Hoje é: {getWeekDay()}</h1>
         </>
     )
 }

94d668fcdafee36b4bc5b2688f4e922e3ad86583
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-08-13 17:14:19 -0300

    children reactNode

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,18 +1,15 @@
 import { Person } from "@/components/person"
+import { Card } from "@/components/card"
 function Page() {
   return (<div>
     <h1 className="bg-amber-600 text-4xl">Olá</h1>
     <h2>mundo</h2>
-    <Person
-      name = "Elon Musk"
-      avatar = "#"
-      roles = {["CEO da Tesla", "CEO da SpaceX"]}
-    />
-    <Person
-      name = "Jeff Bezos"
-      avatar = "#"
-      roles = {["CEO da Amazon", "CEO da Blue Origin"]}
-    />
+    <Card>
+      <>
+        <h3 className="text-3xl font-bold italic">Frase de efeito</h3>
+        <p className="text-right text-sm">Autor Desconhecido</p>
+      </>
+    </Card>
   </div>)
 
 }
--- react-primeiro-projeto/src/components/person.tsx
+++ react-primeiro-projeto/src/components/person.tsx
@@ -1,10 +1,14 @@
 type Props = {
     name:string
-    avatar: string
+    avatar?: string
     roles: string[]
     adress?: string
 }
-export const Person = ({name, avatar, roles}: Props) => {
+export const Person = ({
+    name,
+    avatar = "https://media-for2-2.cdn.whatsapp.net/v/t61.24694-24/483647873_1339299243992472_1626682614502700882_n.jpg?ccb=11-4&oh=01_Q5Aa2QHgCvU6HaQwMyTkmbmSh5sbSKsF2DrUJm64tELxLCVpIQ&oe=68A9DA33&_nc_sid=5e03e0&_nc_cat=111",
+    roles
+}: Props) => {
     return (
         <>
             <h1 style={{color: "red", fontSize: "30px"}} >

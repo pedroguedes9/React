4c28a0941dc3b943fca82fff75fd0ba27279eaa9
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-08-19 21:23:39 -0300

    exclui os arquivos do ultimo exercicio

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,9 +1,8 @@
-import { StudentTable } from "@/components/studentTable"
-import { students } from "@/data/students"
+
+
 function Page() {
   return (
     <div className="container mx-auto ">
-      <StudentTable students={students} />
     </div>
   )
 }

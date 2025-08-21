fc95fea1a7261b0787a0f92c60f7158892714376
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-08-20 21:07:14 -0300

    evento de clique

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,8 +1,15 @@
-
+"use client"
 
 function Page() {
+  function handleClick() {
+      alert("funcionou")
+    }
   return (
-    <div className="container mx-auto ">
+    
+    <div className="container mx-auto w-screen h-screen flex justify-center items-center  ">
+      <button onClick={() => {
+        
+      }} className="p-3 bg-blue-700 text-white rounded-3xl"> clique aqui</button>
     </div>
   )
 }

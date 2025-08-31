9ab438094ccd532d5a1082af827081284cdbadf8
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-08-31 12:45:18 -0300

    spread operator em states de objetos

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,19 +1,34 @@
 "use client"
 
 import { useState } from "react"
-
+type Person = {
+  name: string,
+  lastName: string
+}
 function Page() {
-  const [count, setCount] = useState(0)
-  const handleButtonClick = () => {
-    setCount(prev => prev + 1)
-    setCount(prev => prev + 1)
-    setCount(prev => prev + 1)
-    console.log(count)
+const [fullName, setFullName] = useState<Person>({name: "", lastName: ""})
+const handleClearButton = () => {
+  setFullName({...fullName, name: ""})
   }
   return (
-    <div className="container mx-auto w-screen h-screen flex flex-col justify-center items-center">
-      <p>{count}</p>
-      <button onClick={handleButtonClick} className="bg-blue-500 p-3 rounded-md">+2</button>
+    <div className= "container mx-auto w-screen h-screen flex flex-col justify-center items-center">
+      <input 
+      type="text"
+      placeholder="Nome"
+      className="border border-black p-3 text-2xl text-white rounded-md mb-3"
+      value={fullName.name}
+      onChange={e => setFullName({...fullName, name: e.target.value})}
+      />
+      <input 
+      type="text"
+      placeholder="Sobrenome"
+      className="border border-black p-3 text-2xl text-white rounded-md mb-3"
+      value={fullName.lastName}
+      onChange={e => setFullName({...fullName ,lastName: e.target.value})}
+      />
+      <p>Meu nome completo é:</p>
+      <p>{fullName.name} {fullName.lastName}</p>
+      <button onClick={handleClearButton}>Limpar Nome</button>
     </div>
   )
 }

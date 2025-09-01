82dadd219500a0295dc47a37cec93b5a063f14eb
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-08-31 21:16:14 -0300

    sla oq escrever nessa porra

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,34 +1,9 @@
 "use client"
-
-import { useState } from "react"
-type Person = {
-  name: string,
-  lastName: string
-}
 function Page() {
-const [fullName, setFullName] = useState<Person>({name: "", lastName: ""})
-const handleClearButton = () => {
-  setFullName({...fullName, name: ""})
-  }
+  
   return (
     <div className= "container mx-auto w-screen h-screen flex flex-col justify-center items-center">
-      <input 
-      type="text"
-      placeholder="Nome"
-      className="border border-black p-3 text-2xl text-white rounded-md mb-3"
-      value={fullName.name}
-      onChange={e => setFullName({...fullName, name: e.target.value})}
-      />
-      <input 
-      type="text"
-      placeholder="Sobrenome"
-      className="border border-black p-3 text-2xl text-white rounded-md mb-3"
-      value={fullName.lastName}
-      onChange={e => setFullName({...fullName ,lastName: e.target.value})}
-      />
-      <p>Meu nome completo é:</p>
-      <p>{fullName.name} {fullName.lastName}</p>
-      <button onClick={handleClearButton}>Limpar Nome</button>
+      
     </div>
   )
 }

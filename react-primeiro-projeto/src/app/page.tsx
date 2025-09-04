20dc6efbe327b1c2fa928429081e7878bc2f239a
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-09-04 11:41:26 -0300

    exercício da galeria de fotos completo

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,69 +1,39 @@
 "use client"
 
-type TodoItem = {
-    label: string,
-    checked: boolean,
-    id: number
-}
-
+import { Modal } from "@/components/Modal"
+import { PhotoItem } from "@/components/PhotoItem"
+import { photoList } from "@/data/photoList"
 import { useState } from "react"
 
 function Page() {
-  const [itemImput, setItemInput] = useState("")
-  const [list, setList] = useState<TodoItem[]>([
-    {id: 1, label: "Fazer o dever de casa", checked: false},
-    {id: 2,label: "Estudar", checked: false}
-  ])
-  
-  const handleAddButton = () => {
-    if(itemImput.trim() === "") return
-    else {
-      setList([
-        ...list,
-        {id: list.length + 1 ,label: itemImput, checked: false}
-      ])
-      setItemInput("")
-    }
-  }
-  const deleteItem = (id: number) => {
-    setList(list.filter((item) => item.id !== id))
-  }
-  const toggleItem = (id: number) => {
-    let newList = [...list]
-    for(let i in newList){
-      if(newList[i].id === id){
-        newList[i].checked = !newList[i].checked
+  const [showModal, setShowModal] = useState(false)
+  const [imageOfModal, setImageOfModal] = useState("")
+  const openModal = (id: number) => {
+    for(let i in photoList) {
+      if(photoList[i].id === id) {
+        setImageOfModal(photoList[i].url)
       }
     }
-    setList(newList)
+    setShowModal(true)
   }
-  
+  const closeModal = () => {
+    setShowModal(false)
+  }
   return (
-    <div className="w-screen h-screen flex flex-col items-center text-2xl">
-      <h1 className="text-4xl mt-5">Lista de tarefas</h1>
-      <form onSubmit={e => e.preventDefault()} className="flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-gray-800 gap-3">
-        <input 
-          type="text"
-          placeholder="O que deseja fazer?"
-          className="flex-1 border border-black p-3 text-2xl text-black bg-white rounded-md"
-          value={itemImput}
-          onChange={e => setItemInput(e.target.value)}
-        />
-        <button onClick={handleAddButton}>Adicionar</button>
-      </form>
-      <p className="my-4">{list.length} itens na lista</p>
-      <ul className="w-full max-w-lg list-none pl-5">
-        {list.map((item) => (
-          <li key={item.id}>
-            <input type="checkbox"
-            onChange={() => toggleItem(item.id)}
-            checked={item.checked}
-            className="m-3 w-6 h-6"
-            />
-            {item.label} - <button onClick={() => deleteItem(item.id)} className="hover:underline">[ deletar ]</button>
-          </li>
+    <div className="mx-2 flex flex-col justify-center items-center">
+      <h1 className="text-center text-3xl font-bold my-10">Fotos intergaláticas</h1>
+      <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+        {photoList.map(item => (
+          <PhotoItem 
+          key={item.id}
+          photo={item}
+          onClick={() => {openModal(item.id)} } 
+          />
         ))}
-      </ul>
+      </section>
+      {showModal && (
+        <Modal image={imageOfModal} closeModal={closeModal} />
+      )}
     </div>
   )
 }
---
+++ react-primeiro-projeto/src/data/photoList.ts
@@ -0,0 +1,40 @@
+import { Photo } from "@/types/Photo";
+
+export const photoList: Photo[] = [
+    {
+        id: 1,
+        url: "/assets/1.jpg"
+    },
+    {
+        id: 2,
+        url: "/assets/2.jpg"
+    },
+    {
+        id: 3,
+        url: "/assets/3.jpg"
+    },
+    {
+        id: 4,
+        url: "/assets/4.jpg"
+    },
+    {
+        id: 5,
+        url: "/assets/5.jpg"
+    },
+    {
+        id: 6,
+        url: "/assets/6.jpg"
+    },
+    {
+        id: 7,
+        url: "/assets/7.jpg"
+    },
+    {
+        id: 8,
+        url: "/assets/8.jpg"
+    },
+    {
+        id: 9,
+        url: "/assets/9.jpg"
+    }
+]

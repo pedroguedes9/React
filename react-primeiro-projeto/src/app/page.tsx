f4fe90e6c79d6aad6098eeb9849b8b9fa8a711df
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-09-04 10:30:59 -0300

    finalizando o estudo dos arrays

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -2,46 +2,68 @@
 
 type TodoItem = {
     label: string,
-    checked: boolean
+    checked: boolean,
     id: number
 }
+
 import { useState } from "react"
 
 function Page() {
   const [itemImput, setItemInput] = useState("")
   const [list, setList] = useState<TodoItem[]>([
-    {label: "Fazer o dever de casa", checked: false, id: 1},
-    {label: "Estudar", checked: false, id: 2}
+    {id: 1, label: "Fazer o dever de casa", checked: false},
+    {id: 2,label: "Estudar", checked: false}
   ])
+  
   const handleAddButton = () => {
-    {itemImput ?? 
+    if(itemImput.trim() === "") return
+    else {
       setList([
-            ...list, 
-            {label: itemImput, checked: false, id: list.length + 1 }
-          ])
-      setItemInput("") 
+        ...list,
+        {id: list.length + 1 ,label: itemImput, checked: false}
+      ])
+      setItemInput("")
     }
-    
   }
+  const deleteItem = (id: number) => {
+    setList(list.filter((item) => item.id !== id))
+  }
+  const toggleItem = (id: number) => {
+    let newList = [...list]
+    for(let i in newList){
+      if(newList[i].id === id){
+        newList[i].checked = !newList[i].checked
+      }
+    }
+    setList(newList)
+  }
+  
   return (
-    <div className= "w-screen h-screen flex flex-col items-center text-2xl">
+    <div className="w-screen h-screen flex flex-col items-center text-2xl">
       <h1 className="text-4xl mt-5">Lista de tarefas</h1>
-      <div className=" flex  w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-gray-800 gap-3" >
+      <form onSubmit={e => e.preventDefault()} className="flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-gray-800 gap-3">
         <input 
-        type="text"
-        placeholder="O que deseja fazer?"
-        className="flex-1 border border-black p-3 text-2xl text-black  bg-white rounded-md  "
-        value={itemImput}
-        onChange={e =>  (setItemInput(e.target.value))}
+          type="text"
+          placeholder="O que deseja fazer?"
+          className="flex-1 border border-black p-3 text-2xl text-black bg-white rounded-md"
+          value={itemImput}
+          onChange={e => setItemInput(e.target.value)}
         />
-        <button onClick={handleAddButton} >Adicionar</button>
-      </div>
+        <button onClick={handleAddButton}>Adicionar</button>
+      </form>
       <p className="my-4">{list.length} itens na lista</p>
-      <ul className="w-full max-w-lg list-disc pl-5'">
-        {list.map(item => (
-          <li key={item.id}>{item.label} - <button className="hover:underline">[ deletar ]</button></li>
+      <ul className="w-full max-w-lg list-none pl-5">
+        {list.map((item) => (
+          <li key={item.id}>
+            <input type="checkbox"
+            onChange={() => toggleItem(item.id)}
+            checked={item.checked}
+            className="m-3 w-6 h-6"
+            />
+            {item.label} - <button onClick={() => deleteItem(item.id)} className="hover:underline">[ deletar ]</button>
+          </li>
         ))}
-        </ul>
+      </ul>
     </div>
   )
 }

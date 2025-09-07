cae6155bee5b1efb0d77380f0e6cf0c799410d7c
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-09-07 14:16:36 -0300

    o que são os reducers?

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,17 +1,39 @@
 "use client"
 
-import { Square } from "@/components/Square"
-import { use, useEffect, useState } from "react"
-
-
-
-
+import { Item } from "@/types/Item"
+import { useState } from "react"
 function Page() {
-const [show, setShow] = useState(false)
+  const [list, setList] = useState<Item[]>([])
+  const addNewItem = (text:string) => {
+    setList([...list, {
+      id: list.length,
+      text,
+      done: false
+    } ])
+  }
+  const editItemText = (id: number, newText: string) => {
+    setList(
+      list.map((item) => {
+        if(item.id === id) {
+          item.text = newText
+        }
+        return item
+      })
+    )
+  }
+  const toggleItem = (id:number) => {
+    setList(list.map((item) => {
+      if(item.id === id) {
+        item.done = !item.done
+      }
+      return item
+    }))
+  }
+  const removeItem = (id: number) => {
+    setList(list.filter(item => item.id !== id ))
+  }
   return (
     <div className="">
-      <button className="cursor-pointer" onClick={() => setShow(!show)} >Mostrar/ Ocultar</button>
-      {show && <Square/>}
     </div>
   )
 }

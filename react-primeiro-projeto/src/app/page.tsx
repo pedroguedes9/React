b4d90b1c177fcd7a8aa4ccab0c534dbbced7ac15
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-09-07 12:58:13 -0300

    estudando sobre cleanup

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,24 +1,17 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { Square } from "@/components/Square"
+import { use, useEffect, useState } from "react"
 
 
 
 
 function Page() {
-  const [name, setName] = useState("Pedro")
-  const [age, setAge] = useState(17)
-  useEffect(() => {
-    console.log("roudou")
-  }, [name])
+const [show, setShow] = useState(false)
   return (
     <div className="">
-      <p>Meu nome é {name} e tenho {age} anos</p>
-      <hr />
-      <button onClick={() => {setName("Dudu")}}><p> - Mudar para dudu - </p></button>
-      <button onClick={() => {setName("Pedro")}}> - Mudar para pedro - </button>
-      <button onClick={() => {setAge(10)}}><p> - Mudar para 10 anos - </p></button>
-    <button onClick={() => {setAge(17)}}> - Mudar para 17 anos- </button>
+      <button className="cursor-pointer" onClick={() => setShow(!show)} >Mostrar/ Ocultar</button>
+      {show && <Square/>}
     </div>
   )
 }
